f6da551d74cff039afc80b284d2c6725bb916176
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-21 00:31:31 +0600

    Create Swift.swift

---
+++ Swift.swift
@@ -0,0 +1,7 @@
+import Foundation
+var num1 = Int(readLine()!)!
+var num2 = Int(readLine()!)!
+var num3 = Int(readLine()!)!
+var num4 = Int(readLine()!)!
+var num = num1 * num2 - num3 * num4
+print("DIFERENCA = \(num)")
